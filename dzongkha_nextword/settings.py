e95e800a9a727cbdd01b8eb2fe1b002d78126f68
Yoezer2020 <tenzinoser2@gmail.com>
2024-01-25 12:46:12 +0600

    so i am trying host this project using the vercel app.

--- dzongkha_nextword/settings.py
+++ dzongkha_nextword/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-57i@sh2l*v_-l#)w7ll2fg=-f1&d@j9_zwg3&0@1-8$o3-tt4v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
